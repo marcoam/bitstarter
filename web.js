81912e572bace3213a2149c737abf79bad1624fc
Marco A Marquez <marco.marquez.b@gmail.com>
2013-07-10 03:26:33 +0000

    Keep Modifying index.html

--- web.js
+++ web.js
@@ -3,11 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html');
+  var buffer = new Buffer("Hello World from index.html", 25);
+  var data = fs.readFileSync("index.html", "utf-8");
+  response.send(data);
   //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("I am now listening on " + port);
+  console.log("Listening on " + port);
 });
